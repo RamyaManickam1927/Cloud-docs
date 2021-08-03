57c176be726e692179f4bdd18a06b8df27f56470
RamyaManickam1927 <88128661+RamyaManickam1927@users.noreply.github.com>
2021-08-03 16:00:24 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,5 +9,22 @@ var shelljs = require('shelljs');
  * Source shipping to gitlap
  */
 gulp.task('ship-to-gitlap', function (done) {
-    console.log("ship-to-gitlab");
+  var gitPath="https://gitlab.syncfusion.com/ramya.manickam/test";
+      var clone = shelljs.exec('git clone ' + gitPath + ' -b master'+ ' ' + `./gitlapRepo/docs`, {
+            silent: false
+        });
+  if (clone.code !== 0) {
+            console.log(clone.stderr);
+            done();
+            return;
+        } else {
+            console.log('Clone has been completed...!');
+            shelljs.cp('-rf', `docs/*`, `./gitlapRepo/docs`);
+            shelljs.cd(`./gitlapRepo/docs`);
+            shelljs.exec('git add .');
+            shelljs.exec('git pull');
+            shelljs.exec('git commit -m \"ci-skip(EJ2-000): source updation from github repo [ci skip]\" --no-verify');
+            shelljs.exec('git push');
+            shelljs.cd('../../');
+        }
 });
