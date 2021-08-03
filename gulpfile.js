e304a50e6ea99d499267f0405a3a9417f5b47370
RamyaManickam1927 <88128661+RamyaManickam1927@users.noreply.github.com>
2021-08-03 16:56:55 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var shelljs = require('shelljs');
  * Source shipping to gitlap
  */
 gulp.task('ship-to-gitlap', function (done) {
-  var gitPath="https://gitlab.syncfusion.com/ramya.manickam/test";
+var gitPath = 'https://' + `@gitlab.syncfusion.com/essential-studio/ej2-calendars-angular-docs`;
       var clone = shelljs.exec('git clone ' + gitPath + ' -b master'+ ' ' + `./gitlapRepo/docs`, {
             silent: false
         });
