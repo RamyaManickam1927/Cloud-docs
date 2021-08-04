44f643a840d971254f720812d3d77c56b21805f7
RamyaManickam1927 <88128661+RamyaManickam1927@users.noreply.github.com>
2021-08-04 13:42:32 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('ship-to-gitlap', function (done) {
     console.log('--cloneRepos----' + cloneRepos);    
     
     for (var j = 0; j < cloneRepos.length; j++) {
-        var gitPath = 'https://gitlab.syncfusion.com/bold-reports/cloud-docs/';
+       var gitPath ='https://' + user + ':' + token + `@gitlab.syncfusion.com/bold-reports/cloud-docs/`;
         console.log('Clone has been started...!');
         var clone = shelljs.exec('git clone ' + gitPath + ' -b development' + ' ' + `./gitlapRepo/docs`, {
             silent: false
